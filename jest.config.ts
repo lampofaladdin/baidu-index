0a2af31399c3c4ca384714dd52f32f2b528d058b
aladdin <wangdingding@42how.com>
2024-04-03 19:01:55 +0800

    chore: change jest

--- jest.config.ts
+++ jest.config.ts
@@ -174,7 +174,7 @@ export default {
 
     // A map from regular expressions to paths to transformers
     transform: {
-        '^.+\\.(t|j)s$': 'ts-jest',
+        '^.+\\.(t)s$': 'ts-jest',
     },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
